65ec1cb5f4d3759b242fb35beec761a3a31a74ac
Mitchell Schepps <maschepps@ucla.edu>
2023-08-24 13:45:24 -0700

    Erase the double on GWO from the test run

--- R/GWO.Algorithm.R
+++ R/GWO.Algorithm.R
@@ -97,7 +97,7 @@
 #' https://doi.org/10.1016/j.advengsoft.2013.12.007
 #' @export
 
-GWO <- function(FUN, optimType="MIN", numVar, numPopulation=40, maxIter=500, rangeVar, c_length = c_length, c_value = c_value, c_length, c_value){
+GWO <- function(FUN, optimType="MIN", numVar, numPopulation=40, maxIter=500, rangeVar, c_length = c_length, c_value = c_value){
 	# calculate the dimension of problem if not specified by user
 	dimension <- ncol(rangeVar)
 
